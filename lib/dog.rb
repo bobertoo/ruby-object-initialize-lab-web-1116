758ab078f427b7186517052ca159170a406c98cb
Vinny Martinez <vinny@vinnymartinez.com>
2016-11-30 11:33:09 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+class Dog
+  def initialize(set_name, set_breed="Mutt")
+    @name = set_name
+    @breed = set_breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,5 @@
+class Person
+  def initialize(set_name)
+    @name = set_name
+  end
+end
